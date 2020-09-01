c1204387551b085f007a101a03eb06c65260f45d
Pat  Pignataro <pat6284@msn.com>
2020-09-01 13:14:30 +0000

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,11 @@
+const timerElement = document.getElementById("counter")
+const upButton = document.getElementById("plus");
+const downButton = document.getElementById("minus");
+const pauseButton = document.getElementById("pause")
+const likeButton = document.getElementById("heart")
+const submitElement = document.getElementById("submit")
+const listComments = document.getElementById("list")
+
+let likeCount = 0;
+let secondsLeft = 0;
+let isPaused = false
