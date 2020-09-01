71a452d09fd0933578c6d64f5e53630576dc2773
Pat  Pignataro <pat6284@msn.com>
2020-09-01 13:20:20 +0000

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -16,7 +16,52 @@ let timerInterval = setInterval(function () {
 }, 1000);
 
 upButton.addEventListener("click", () => {
-    console.log("Worked")
+    console.log("Works")
     secondsLeft = parseInt(secondsLeft) + 1;
     timerElement.textContent = secondsLeft;
+});
+
+downButton.addEventListener("click", () => {
+    console.log("Works")
+    secondsLeft = parseInt(secondsLeft) - 1;
+    timerElement.textContent = secondsLeft;
+});
+
+pauseButton.addEventListener("click", () => {
+    if (!isPaused) {
+        clearInterval(timerInterval);
+        upButton.setAttribute("disabled", "disabled") ;
+        downButton.setAttribute("disabled", "disabled");
+        likeButton.setAttribute("disabled", "disabled");
+        pauseButton.innerText = "resume";
+        isPaused = true
+    }
+    else {
+        isPaused = false
+        timerInterval = setInterval( function() {
+            secondsLeft++;
+            timerElement.textContent = secondsLeft;
+        }, 1000);
+        upButton.removeAttribute("disabled")
+        downButton.removeAttribute("disabled")
+        likeButton.removeAttribute("disabled")
+        pauseButton.innerText = "pause";
+    }
+});
+
+likeButton.addEventListener("click", function() {
+    console.log("Works)
+    let likes = document.querySelector(".likes") 
+    likeCount += 1;
+    likes.innerText = likeCount
+    console.log(this.innerText)
+
+});
+
+submitElement.addEventListener("click", function () {
+    event.preventDefault();
+    console.log("it's Working");
+    var response = "This is fun.";
+    listComments.textContent = response;
+
 });
